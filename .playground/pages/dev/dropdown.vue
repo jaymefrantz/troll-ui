61e3fcd03e34d100490e5387a9145fa78071a648
Jayme Frantz <hello@jaymefrantz.com>
2024-05-11 14:48:38 -0400

    initial dropdown dev

---
+++ .playground/pages/dev/dropdown.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+<Dropdown :options="options" label="year"/>
+</template>
+
+<script setup lang="ts">
+  const options = [
+    { label: "Any year", value: "" },
+    { label: "2024", value: "2024" },
+    { label: "2023", value: "2023" },
+    { label: "2022", value: "2022" },
+    { label: "2021", value: "2021" },
+    { label: "2020", value: "2020" },
+    { label: "2019", value: "2019" },
+    { label: "2018", value: "2018" },
+    { label: "2017", value: "2017" },
+    { label: "2016", value: "2016" },
+    { label: "2015", value: "2015" },
+    { label: "2014", value: "2014" },
+    { label: "2013", value: "2013" },
+  ]
+
+</script>
+
+<style lang="scss">
+
+.dropdown-inner-wrap::-webkit-scrollbar-thumb {
+  border-radius: 4px;
+}
+
+.dropdown-outer-wrap {
+  border-radius: 1em;
+  border: 1px solid #f7f7f7;
+  box-shadow: 0.15rem 0.225rem 0 rgb(0 0 0 / 10%);
+}
+
+.dropdown-trigger, .dropdown-mobile-select-container {
+  box-shadow: 0.15rem 0.225rem 0 rgb(0 0 0 / 10%);
+  border-radius: 1.75rem;
+  border: 1px solid #f7f7f7;
+
+}
+
+.dropdown-select-input {
+  border-radius: 1.75rem;
+}
+//--option-hover-background: $lightest-magenta
+// --option-selected-background: map-get($magenta, shadow)
+//need font weight
+.dropdown-mobile-select-container {
+  padding-right: 2rem;
+  padding-left: 0.25rem;
+}
+
+.dropdown-list > li {
+  border-radius: 0.2em; 
+}
+
+</style>
---
+++ components/Dropdown.vue
@@ -0,0 +1,425 @@
+<template lang="pug">
+div(ref="container").custom-dropdown
+	label(v-if="label !== ''" v-html="label" :id="`${id}-label`" @click="focusDropdown").default-label
+	div.dropdown-mobile-select-container
+		select(v-model="selectedValue" @change="selectChanged"  ref="dropdownSelect" :aria-describedby="`${id}-label`").dropdown-select-input
+			option(v-for="(option, index) in dropdownOptions" :key="option.value" :value="option.value") {{option.label}}
+		<Icon :size="iconSize" :name="icon" class="dropdown-arrow"/>
+	button(type="button" ref="dropdownTrigger" :aria-describedby="`${id}-label`" role="combobox" aria-haspopup="listbox" :aria-controls="id" :aria-activedescendant="selectedValue" :aria-expanded="expanded ? 'true' : 'false'"
+		@keydown.down.exact.prevent="goDown" 
+		@keydown.up.exact.prevent="goUp" 
+		@keydown.home.prevent="selectFirst" 
+		@keydown.end.prevent="selectLast" 
+		@keydown.alt.down.exact.prevent="!expanded ? openDropdown($event) : closeDropdown($event)" 
+		@keydown.alt.up.exact.prevent="!expanded ? openDropdown($event) : closeDropdown($event)"
+		@keydown.space.exact="() => { wasSpace = true }"
+		@click="!expanded ? openDropdown($event) : closeDropdown($event)"
+		@keydown.tab="() => { wasReservedKey = true }"
+		@keydown="keydown"
+	).dropdown-trigger
+		span.dropdown-trigger-label {{selectedLabel}}
+		<Icon :name="icon" class="dropdown-arrow"/>
+	div(v-show="expanded" :class="{ 'end': isEnd }" ref="outer").dropdown-outer-wrap
+		div(ref="inner").dropdown-inner-wrap
+			ul(:id="id" role="listbox" v-scroll="[dropdownScroll, { 'throttle' : 200 }]" ref="dropdownList").dropdown-list
+				li(v-for="(option, index) in dropdownOptions" ref="listItemOptions" class="dropdown-option" role="option" tabindex="0" :id="`${id}-${option.value}`" 
+					:aria-posinset="index + 1" 
+					:aria-setsize="dropdownOptions.length + 1" 
+					:aria-selected="option.value === selectedValue ? 'true' : 'false'"
+					@keydown.space.exact.prevent="spaceHit"
+					@click="selectOption(option)"
+					@keydown.enter.exact.prevent="selectOption(option)"
+					@keydown.escape.exact.prevent="closeDropdown"
+					@keydown.alt.up.exact.prevent="closeDropdown" 
+					@keydown.alt.down.exact.prevent="closeDropdown"
+					@keydown.tab.exact="closeDropdown"
+					@keydown.shift.tab.exact="closeDropdown" 
+					@keydown.up.exact.prevent="goUp"
+					@keydown.down.exact.prevent="goDown"
+					@keydown.home.exact.prevent="selectFirst" 
+					@keydown.end.exact.prevent="selectLast"
+					@keydown="keydown"
+				)
+					span.option-label {{option.label}}
+					span(:aria-hidden="option.value === selectedValue ? 'false' : 'true'").sr-only , Selected
+</template>
+
+<script setup lang="ts">
+	import { onClickOutside, useScroll } from "@vueuse/core"
+	import { vScroll } from '@vueuse/components'
+	const isMobile = useBreakpoints()?.smallerOrEqual("dropdown") ?? false
+	const id = useId()
+	const selectedValue = ref("")
+	const expanded = ref(false)
+	const previouslySelected = ref("")
+	const typedText = ref("")
+	const lastTyped = ref<Date>(new Date())
+	const wasReservedKey = ref(false)
+	const wasSpace = ref(false)
+	const test = ref("") //needed
+	const isEnd = ref(false)
+	const container = ref<null | HTMLDivElement>(null)
+	const dropdownSelect = ref<null | HTMLSelectElement>(null)
+	const dropdownTrigger = ref<null | HTMLButtonElement>(null)
+	const dropdownList = ref<null | HTMLUListElement>(null)
+	const listItemOptions = ref<null | HTMLLIElement[]>(null)
+	const { arrivedState } = useScroll(dropdownList)
+	const { bottom } = toRefs(arrivedState)
+
+	onClickOutside(container, () => expanded.value = false)
+
+	const emit = defineEmits<{
+		(e: "change", value: string): void
+	}>()
+
+	interface Option {
+		label: string
+		value: string
+	}
+
+	const props = withDefaults(
+		defineProps<{
+			override?: string
+			options: Option[]
+			label?: string
+			icon?: string
+			iconSize?: string
+		}>(),
+		{
+			label: "",
+			icon: "material-symbols:keyboard-arrow-down-rounded",
+			iconSize: "1.25em"
+		}
+	)
+
+	const { override, options: dropdownOptions, label } = toRefs(props)
+	selectedValue.value = props?.override ?? dropdownOptions.value[0].value	
+
+	function selectChanged() {
+		emit("change", selectedValue.value)
+	}
+
+	const selectedLabel = computed(() => {
+		if (dropdownOptions.value === null) return ""
+		return dropdownOptions.value.find(option => option.value === selectedValue.value)?.label ?? ""
+	})
+
+	function openDropdown(event: Event) {
+		if (wasSpace.value) {
+			spaceHit(event)
+			return
+		}
+		expanded.value = true
+		previouslySelected.value = selectedValue.value
+
+		const index = dropdownOptions.value.findIndex(option => option.value === selectedValue.value)
+		setTimeout(() => {
+			listItemOptions.value[index].focus()
+		}, 10)
+	}
+	function closeDropdown() {
+		expanded.value = false
+		wasReservedKey.value = false //this may not be great logic?
+
+		setTimeout(() => {
+			dropdownTrigger.value.focus()
+		}, 1)
+
+		if (previouslySelected.value !== selectedValue.value) selectChanged()
+	}
+
+	function spaceHit({ target }: Event) {
+		wasSpace.value = false
+		const typedTime = new Date()
+		const difference = typedTime - lastTyped.value
+
+		if (difference > 1000 || typedText.value.trim() === "") {
+			wasReservedKey.value = true
+			if (target.classList.contains("dropdown-trigger")) {
+				!expanded.value ? openDropdown() : closeDropdown()
+			} else {
+				closeDropdown()
+			}
+		} else {
+			wasReservedKey.value = false
+		}
+	}
+
+	function focusDropdown() {
+		if (isMobile) {
+			dropdownSelect.value?.focus()
+		} else {
+			dropdownTrigger.value?.focus()
+		}
+	}
+
+	function selectOption({ label, value }: Option) {
+		selectedValue.value = value
+		closeDropdown()
+	}
+
+	function setSelected(target: HTMLElement, index: number) {
+		//event.preventDefault()
+		selectedValue.value = dropdownOptions.value[index].value
+
+		if (target.classList.contains("dropdown-trigger")) {
+			selectChanged()
+		} else {
+			listItemOptions.value[index].focus()
+		}
+	}
+
+	function goDown({ target }: Event) {
+		wasReservedKey.value = true
+		const index = dropdownOptions.value.findIndex(option => option.value === selectedValue.value)
+		if (index + 1 >= dropdownOptions.value.length) return
+		setSelected(target, index + 1)
+	}
+
+	function goUp({ target }: Event) {
+		wasReservedKey.value = true
+		const index = dropdownOptions.value.findIndex(option => option.value === selectedValue.value)
+		if (index <= 0) return
+		setSelected(target, index - 1)
+	}
+
+	function selectFirst({ target }: Event) {
+		wasReservedKey.value = true
+		setSelected(target, 0)
+	}
+
+	function selectLast({ target }: Event) {
+		wasReservedKey.value = true
+		setSelected(target, dropdownOptions.value.length - 1)
+	}
+
+	async function keydown(event: Event) {
+		//this.test = event.key
+		await wait(1)
+
+		if (!wasReservedKey.value) {
+			const key = event.key.toLowerCase()
+			let duplicate = JSON.parse(JSON.stringify(dropdownOptions.value))
+			const index = dropdownOptions.value.findIndex(option => option.value === selectedValue.value)
+			let searchable = []
+			const typedTime = new Date()
+			const difference = typedTime - lastTyped.value
+
+			if (difference < 1000) {
+				typedText.value += key
+				test.value = typedText.value
+				searchable = [...duplicate.splice(index), ...duplicate]
+			} else {
+				typedText.value = key
+				test.value = typedText.value
+				searchable = [...duplicate.splice(index + 1), ...duplicate]
+			}
+
+			const foundItem = searchable.find(({ label }) => label.toLowerCase().startsWith(typedText.value))
+			if (typeof foundItem !== "undefined") {
+				selectedValue.value = foundItem.value
+				const index = dropdownOptions.value.findIndex(option => option.value === selectedValue.value)
+				listItemOptions.value[index].focus()
+			}
+
+			lastTyped.value = typedTime
+		} else {
+			typedText.value = ""
+		}
+
+		await wait(10)
+		wasReservedKey.value = false
+	}
+
+	const dropdownScroll = (e: Event) => {
+		isEnd.value = bottom.value
+	}
+
+</script>
+
+<style lang="scss" scoped>
+	.custom-dropdown {
+    position: relative;
+    display: inline-block;
+  }
+
+	.dropdown-trigger {
+    --focus-offset: 2px;
+    padding: var(--trigger-padding, 0.95em 1em 0.95em 1.5em);
+    font-size: var(--trigger-font, 0.85em);
+    letter-spacing: 0.075em;
+
+    &:hover:not(:focus) {
+      color: var(--trigger-hover-color, $dark-grey);
+    }
+
+    &[aria-expanded="true"] {
+      & + .dropdown-outer-wrap {
+        opacity: 1;
+      }
+    }
+
+    &[aria-expanded="true"] {
+      .dropdown-arrow {
+        transform: rotate(180deg);
+      }
+    }
+
+    @include viewport($dropdown-mobile-viewport down) {
+      .custom-dropdown & {
+        display: none;
+      }
+    }
+  }
+
+  .dropdown-arrow {
+    width: v-bind(iconSize);
+    margin-left: var(--arrow-left, 0.5em);
+    display: block;
+    transition: transform 0.275s ease-in-out;
+  }
+
+  .dropdown-trigger-label {
+    line-height: 1.4;
+    display: block;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  .dropdown-mobile-select-container {
+    position: relative;
+    color: var(--dropdown-color, $grey);
+
+    .dropdown-arrow {
+      position: absolute;
+      top: 50%;
+      transform: translateY(-50%);
+			width: v-bind(iconSize);
+      right: var(--dropdown-container-padding-right, 1em);
+    }
+
+    @include viewport($dropdown-mobile-viewport up) {
+			#main & {
+      	display: none;
+			}
+    }
+  }
+
+  .dropdown-select-input {
+    --focus-offset: 2px;
+    display: block;
+    width: 100%;
+    border: 0;
+    -webkit-appearance: none;
+		padding: var(--trigger-padding, 0.95em 1em 0.95em 1.5em);
+		padding-right: calc(v-bind(iconSize) + var(--dropdown-container-padding-right, 1em) + 0.5em);
+    font-family: var(--trigger-font, arial);
+		font-size: var(--trigger-font-size, 0.85em);
+    letter-spacing: 0.075em;
+  }
+
+  .dropdown-list {
+    max-height: var(--dropdown-height, 10rem);
+
+    & > li {
+      padding: var(--option-padding, 0.2em 0.75em 0.15em);
+      margin: var(--option-margin, 0.2em 0.35em 0.2em 0.1em);
+      transition: all 0.15s ease-in-out;
+      letter-spacing: 0.02em;
+
+      &[aria-selected="true"] {
+        background-color: var(--option-selected-background, $grey);
+        color: var(--option-selected-color, white);
+      }
+
+      &:not([aria-selected="true"]):hover {
+        background-color: var(--option-hover-background, $dark-grey);
+        color: var(--option-hover-color, white);
+      }
+    }
+  }
+
+	//jlf this can maybe be abstracted out?
+	.default-label {
+    font-size: 0.825em; 
+    margin-left: 1em;
+    margin-bottom: 0.5em;
+    display: block;
+  }
+
+	.dropdown-trigger, .dropdown-mobile-select-container {
+    background-color: var(--dropdown-color, white);
+		border: var(--dropdown-border, 1px solid $grey);
+    color: var(--dropdown-color, $grey);
+    text-align: left;
+    position: relative;
+    display: flex;
+    width: 100%;
+    align-items: center;
+    transition: color $fast;
+    justify-content: space-between;
+		font-family: var(--trigger-font, arial);
+		font-size: var(--trigger-font-size, 0.85em);
+  }
+
+	.dropdown-option {
+		font-size: var(--option-size, 0.9em);
+    color: var(--option-color, $grey);
+	}
+
+	.dropdown-outer-wrap {
+    background-color: var(--dropdown-background, white);
+    padding: var(--dropdown-outer-padding, 0.5em 0.35em);
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    z-index: 20;
+    transition: all $fastest ease-in-out;
+    margin-top: 0.5em;
+
+
+    &:after {
+      content: "";
+      position: absolute;
+      bottom: 0.25em;
+      left: 0.5em;
+      width: calc(100% - 1.5em);
+      height: 2.5em;
+      background: linear-gradient(-180deg, rgba(255, 255, 255, 0) 0%, rgba(255, 255, 255, 1) 100%);
+      transition: opacity 0.15s;
+      opacity: 1;
+      pointer-events: none;
+    }
+
+    &.end:after {
+      opacity: 0;
+    }
+  }
+
+	.dropdown-inner-wrap {
+		padding: var(--dropdown-inner-padding, 0.35rem 0.35rem 0.25rem 0);
+
+    & > ul {
+      overflow-y: auto;
+
+      & > li {
+        cursor: pointer;
+        color: var(--option-color, $grey);
+
+        &:first-child {
+          margin-top: 0.5rem;
+        }
+      }
+
+      &::-webkit-scrollbar {
+        width: var(--scollbar-width, 5px); 
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background: var(--scrollbar-color, $grey);
+      }
+    }
+  }
+</style>
