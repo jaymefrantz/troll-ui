b6dc1b0e7590bcd050862bcbeb6578ecab907846
Jayme Frantz <hello@jaymefrantz.com>
2024-05-11 14:48:17 -0400

    building out playground app.vue a little more

--- .playground/app.vue
+++ .playground/app.vue
@@ -1,7 +1,10 @@
 <template lang="pug">
   main#main
-    nuxt-link(v-if="!isHome" to="/") Back to home
-    <NuxtPage />
+    div.extra-large-center-margin-wrap
+      nuxt-link(v-if="!isHome" to="/").back-link Back to home
+      div.page
+        h1(v-if="!isHome") {{ title }}
+        <NuxtPage />
   </template>
   
   <script setup lang="ts">
@@ -9,6 +12,21 @@
       return useRoute().path === "/"
     })
 
+    const title = computed(() => {
+      return `${useRoute().path.split("/").at(-1).toLowerCase().split('-').map(function (word) {
+          return (word.charAt(0).toUpperCase() + word.slice(1));
+      }).join(' ')} Demo`
+    })
   </script>
   
-  <style lang="scss" scoped></style>
+  <style lang="scss" scoped>
+    #main {
+      padding: clamp(2.5rem, 5vw, 5rem) clamp(1rem, 5vw, 2rem);
+    }
+
+    .back-link {}
+    h1 {
+      margin-bottom: 2rem;
+    }
+
+  </style>
