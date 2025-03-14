0ae20977a11b07204f0e30229fd57a2899e0564e
Jayme Frantz <hello@jaymefrantz.com>
2025-03-13 21:13:29 -0400

    toggle component

---
+++ components/Toggle.vue
@@ -0,0 +1,107 @@
+<template lang="pug">
+  div(:class="labelPosition").toggle-container
+    input(:id="id" :disabled="disabled" type="checkbox" v-model="boolean")
+    label(:for="id").toggle-label
+      span(v-if="labelPosition === 'top'" v-html="text").toggle-text
+      div.custom-toggle
+        span(v-html="booleanLabel").custom-toggle-knob
+      span(v-if="labelPosition === 'side'" v-html="text").toggle-text
+</template>
+
+<script setup lang="ts">
+  const boolean = defineModel()
+  type BooleanLabels = {
+    [key: string]: string
+  }
+
+  const props = withDefaults(
+    defineProps<{
+      text: string
+      disabled?: boolean
+      id?: string
+      booleanLabels?: BooleanLabels
+      labelPosition?: string
+    }>(),
+    {
+      disabled: false,
+      labelPosition: "side",
+      booleanLabels: {
+        true: "",
+        false: "",
+      },
+    }
+  )
+
+  const id = computed(() => {
+    return props?.id || `Toggle-${useId()}`
+  })
+
+  const booleanLabel = computed(() => {
+    return props.booleanLabels[boolean?.value.toString() ?? "false"]
+  })
+</script>
+
+<style lang="scss" scoped>
+  .toggle-container {
+    position: relative;
+    display: inline-block;
+  }
+
+  input[type="checkbox"] {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    padding: 0;
+    margin: 0;
+    opacity: 0;
+    cursor: pointer;
+    z-index: 3;
+
+    &:checked + label .custom-toggle-knob {
+      transform: translateX(0);
+    }
+
+    &:focus-visible {
+      outline: none;
+
+      & + label .custom-toggle {
+        outline: var(--focus-weight) solid var(--focus-outline-color);
+        outline-offset: var(--focus-offset);
+      }
+    }
+  }
+
+  .toggle-label {
+    display: flex;
+    gap: var(--toggle-gap, 0.65rem);
+
+    .top & {
+      flex-direction: column;
+    }
+  }
+
+  .toggle-text {
+  }
+
+  .custom-toggle {
+    position: relative;
+    background: var(--toggle-background, $grey);
+    border: var(--toggle-border, 1px solid $dark-grey);
+    border-radius: var(--toggle-border-radius, 2rem);
+    padding: var(--toggle-padding-y, 0.25rem) var(--toggle-padding-x, 0.25rem);
+    width: var(--toggle-width, 2.5em);
+    display: flex;
+    align-items: center;
+  }
+
+  .custom-toggle-knob {
+    display: inline-block;
+    width: var(--toggle-knob-width, 50%);
+    background: var(--toggle-knob-background, white);
+    border: var(--toggle-knob-border, $grey);
+    border-radius: var(--toggle-knob-border-radius, 2rem);
+    aspect-ratio: 1;
+    transform: translateX(100%);
+    transition: transform $medium-fast;
+  }
+</style>
