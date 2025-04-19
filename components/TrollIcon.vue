b81ea49a227be267cdd34a95bb52c3d58bcfd338
Jayme Frantz <hello@jaymefrantz.com>
2025-04-19 12:57:24 -0400

    trollIcon w/ wrappers + jsonToCSSVars util

---
+++ components/TrollIcon.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+  div.icon-container
+    <Icon :name="name" :size="size"/>
+    <span v-if="label" class="icon-text" v-html="label"></span>
+</template>
+
+<script setup lang="ts">
+  defineProps({
+    label: {
+      type: String,
+    },
+    name: {
+      type: String,
+    },
+    size: {
+      type: String,
+      default: "none",
+    },
+    color: {
+      type: String,
+      default: "currentColor",
+    },
+  })
+</script>
+
+<style lang="scss" scoped></style>
